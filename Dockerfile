9985c320673117c3132b161c7d6f5a9fb431e104
Ari <arashkermanikolankeh@Arashs-MBP.local>
2022-03-01 17:50:20 +0100

    port added

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.8-slim-buster
 
 WORKDIR /app
 
-EXPOSE 8001
+EXPOSE 8008
 
 ENV FLASK_ENV=development
 
--- app.py
+++ app.py
@@ -3,4 +3,4 @@ from app import app
 if __name__ == "__main__":
    # print("App is running on port http://127.0.0.1:8000/")
     
-    app.run(port=8000) #, port=8000
+    app.run(port=8008) #, port=8000
